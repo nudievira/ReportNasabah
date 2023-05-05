99f8746bccbd60cc16777214b954e4d18faeb044
nudievira <75774658+nudievira@users.noreply.github.com>
2023-05-05 09:44:28 +0700

    Update navbar.blade.php

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -31,15 +31,15 @@
       <!-- Notifications Dropdown Menu -->
       <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-user"></i> {{$online->Name}}
+          <i class="far fa-user"></i>
         </a>
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <a href="{{route('profile-index', ['id' => $online->id])}}" class="dropdown-item">
+          <a href="" class="dropdown-item">
             <i class="fas fa-user-cog mr-2"></i> Profile
           </a>
           
           <div class="dropdown-divider"></div>
-          <a href="{{route('auth-logout')}}" class="dropdown-item">
+          <a href="" class="dropdown-item">
             <i class="fas fa-sign-out-alt mr-2"></i> Logout
           </a>
         </div>
